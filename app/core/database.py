24a2fc82367c6555fda680e3691727e56d64c745
stezele <stezeled.cloud@outlook.com>
2025-02-11 14:02:15 -0600

    update database.py and check the folder structure

--- app/core/database.py
+++ app/core/database.py
@@ -1,15 +1,19 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
+from sqlalchemy import create_engine
+from app.core.config import settings  # Ensure this import is correct
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-# Ensure the connection string is correctly set
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable is not set")
-
-# Create the SQLAlchemy engine
-engine = create_engine(DATABASE_URL, echo=True, fast_executemany=True)
+# Create a database engine
+engine = create_engine(settings.DATABASE_URL)
 
 # Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Dependency function for database session
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  # Provide session
+    finally:
+        db.close()  # Close session after use
--- app/routes/metrics.py
+++ app/routes/metrics.py
@@ -10,21 +10,21 @@ def hired_by_quarter():
     """
     Returns the number of employees hired per job and department in 2021, divided by quarter.
     """
-    query = text(
-        SELECT
+    query = text("""
+    SELECT
         d.department,
         j.job,
         SUM(CASE WHEN MONTH(he.datetime) BETWEEN 1 AND 3 THEN 1 ELSE 0 END) AS Q1,
         SUM(CASE WHEN MONTH(he.datetime) BETWEEN 4 AND 6 THEN 1 ELSE 0 END) AS Q2,
         SUM(CASE WHEN MONTH(he.datetime) BETWEEN 7 AND 9 THEN 1 ELSE 0 END) AS Q3,
         SUM(CASE WHEN MONTH(he.datetime) BETWEEN 10 AND 12 THEN 1 ELSE 0 END) AS Q4
-        FROM hired_employees he
-        JOIN departments d ON he.department_id=d.id
-        JOIN jobs j ON he.job_id=j.id
-        WHERE YEAR(he.datetime)=2021
-        GROUP BY d.department, j.job
-        ORDER BY d.department, j.job
-    )
+    FROM hired_employees he
+    JOIN departments d ON he.department_id = d.id
+    JOIN jobs j ON he.job_id = j.id
+    WHERE YEAR(he.datetime) = 2021
+    GROUP BY d.department, j.job
+    ORDER BY d.department, j.job
+    """)
     with engine.connect() as connection:
         result = connection.execute(query)
         rows = [dict(row) for row in result]
@@ -36,7 +36,7 @@ def departments_above_avg():
     """
     Returns a list of departments that hired more employees than the average in 2021.
     """
-    query = text(
+    query = text("""
         WITH hires AS(
             SELECT d.id, d.department, COUNT(he.id) AS hired
             FROM departments d
@@ -50,7 +50,7 @@ def departments_above_avg():
         FROM hires h, avg_hires a
         WHERE h.hired > a.avg_hired
         ORDER BY h.hired DESC
-    )
+   """)
     with engine.connect() as connection:
         result = connection.execute(query)
         rows = [dict(row) for row in result]
